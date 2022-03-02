7943fdd261475b540fb416047deee3f3117b6000
AhmetErenLaçinbala <ahmetlacinbala@gmail.com>
2022-03-02 20:35:49 +0300

    commit

--- 11_calculate_money_in_leetcode_bank.js
+++ 11_calculate_money_in_leetcode_bank.js
@@ -4,11 +4,26 @@
  */
 
 var totalMoney = function(n){
-    let startValue = 0;
+    let startValue = 1;
+    let money = 0;
     while (n>0){
-        if (n<7){
-            for (let i = startValue; i < startValue )
+        if (n<8){
+            for (let i = startValue; i < startValue + n; i++ ){
+                money += i;
+            }
+            return money;
         }
-        for(let i = 0; i)
+        else{
+            for(let i = startValue; i < startValue + 7; i++){
+                money += i;
+                n--;
+            }
+
+        }
+        startValue++;
     }
+    return money;
 }
+
+
+console.log(totalMoney(8));
